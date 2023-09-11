2e694d07b0a4ef08cc8f7931ef45ae1ea2f252bf
marcelldac <dacmarcell@proton.me>
2023-09-11 16:36:16 -0300

    feat: add responsibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,21 @@
 /* eslint-disable react/prop-types */
-import { Box, Text } from "@chakra-ui/react";
+import {Box, Text} from "@chakra-ui/react";
 
 const Title = (props) => {
-  return <Text as='h1' color='whiteAlpha.900' m={10}>{props.t}</Text>
+  return <Text fontSize='30' color='whiteAlpha.900' m={10}>{props.t}</Text>
 }
 
 const Subtitle = (props) => {
-  return <Text fontSize='17' color='whiteAlpha.900' ml={12}>{props.t}</Text>
+  return <Text fontSize='17' color='whiteAlpha.900' ml={12} mt={2}>{props.t}</Text>
 }
 
 export default function Home(){
   return(
-    <Box bgColor='gray.700' w='80em'>
+    <Box bgColor='gray.700' w={['400px','800px','1300px']}>
       <Title t='Who am i?'/>
-      <Subtitle t='Marcell. 19 y/o developer, linux lover and bitcoiner!'/>
+      <Subtitle t='Marcell. 19 y/o developer, Linux lover and bitcoiner!'/>
       {/* TODO: Colocar link do projeto unicripto (direcionar pro projeto dentro do site ou pro site da Unicripto que será criado) */}
       <Subtitle t='Currently working on UniCripto as CEO'/>
-
     </Box>
   )
 }
