85dba2169ce56461af66f000c873cf950a135cba
marcelldac <dacmarcell@proton.me>
2023-09-11 18:19:53 -0300

    feat: add project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,51 @@
-export default function Project(){
-  return(
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import { 
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Image,
+  Text,
+  useDisclosure,
+
+} from "@chakra-ui/react";
+
+export default function Project(props) {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const size = 'xl';
+
+  let aux = props.skills;
+
+  return (
     <>
-      title
-      description
-      image
+      <Button onClick={() => onOpen()} m={4}>{props.title}</Button>
+      <Drawer onClose={onClose} isOpen={isOpen} size={size}>
+        <DrawerOverlay />
+        {/* TODO: Change bg color above */}
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader textAlign='center'>{props.title}</DrawerHeader>
+          <DrawerBody>
+            <Flex>
+              <Image src={props.src} fallbackSrc='https://via.placeholder.com/150' boxSize='30em'/>
+              <Text ml={3}>
+                {props.description}
+              </Text>
+            </Flex>
+            <Flex mt={2} justify='center'>
+              {Object.keys(aux).map(function(key){
+                return <Image pl={1} src={aux[key]}/>
+              })}
+            </Flex>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,29 @@
+import {Box} from "@chakra-ui/react";
 import Project from "../components/Project";
 
 export default function Projects(){
+
+  const skills = {
+    "react_native": "https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+    "javascript": "https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E",
+  }
+
+  const src = {
+    p1: "https://github-production-user-asset-6210df.s3.amazonaws.com/83461178/264431707-70fdf994-3127-4722-ac50-fd2f687c9752.png"
+  }
+
+  const description = {
+    p1: "A beginner's guide to React Native for those who want to start with the framework. React Native was the first framework I worked with. Initially, I encountered some difficulties in understanding important topics like Async Storage and Redux Saga. I created this guide so that people don't feel as lost as I did when I started."
+  }
+
   return(
-    <Project/>
+    <Box bgColor='gray.700' w={['400px','800px','1300px']} display='flex' justifyContent='center'>
+      <Project title='React Native Guide' src={src.p1} description={description.p1} skills={[skills.react_native, skills.javascript]}/>
+      {/* Q-coder */}
+      {/* Finance System API */}
+      {/* Inventory Control System */}
+      {/* React Native Health App */}
+      {/* Thumbnail Downloader */}
+    </Box>
   )
 }
