a95e656b9291642ba3b4afe25b4c146afd79eb3b
marcelldac <dacmarcell@proton.me>
2023-09-11 16:35:00 -0300

    feat: add projects route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 
 import Home from '../pages/Home';
+import Projects from "../pages/Projects";
 
 export default function Router(){
   return(
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/projects" element={<Projects/>}/>
       </Routes>
     </>
   )
