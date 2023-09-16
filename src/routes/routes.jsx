8765e7cc1cde4912e88cad865de2315349aad1c6
marcelldac <dacmarcell@proton.me>
2023-09-16 08:56:22 -0300

    feat: add skills route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom"
 
 import Home from '../pages/Home';
 import Projects from "../pages/Projects";
+import Skills from "../pages/Skills";
 
-export default function Router(){
-  return(
+export default function Router() {
+  return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path='/skills' element={<Skills />} />
       </Routes>
     </>
   )
