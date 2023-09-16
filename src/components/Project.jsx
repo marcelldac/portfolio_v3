ab057aa0e48442277d17776e9c245b9f812d3807
marcelldac <dacmarcell@proton.me>
2023-09-16 05:17:17 -0300

    feat(Project): modify items

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable react/prop-types */
-import { 
+import {
   Button,
   Drawer,
   DrawerBody,
@@ -12,12 +10,13 @@ import {
   Image,
   Text,
   useDisclosure,
-
+  Box,
+  Link,
 } from "@chakra-ui/react";
 
 export default function Project(props) {
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const size = 'xl';
 
   let aux = props.skills;
@@ -27,21 +26,24 @@ export default function Project(props) {
       <Button onClick={() => onOpen()} m={4}>{props.title}</Button>
       <Drawer onClose={onClose} isOpen={isOpen} size={size}>
         <DrawerOverlay />
-        {/* TODO: Change bg color above */}
-        <DrawerContent>
+        <DrawerContent bg='gray.200'>
           <DrawerCloseButton />
-          <DrawerHeader textAlign='center'>{props.title}</DrawerHeader>
+          <DrawerHeader textAlign='center' fontSize='26'>{props.title}</DrawerHeader>
           <DrawerBody>
-            <Flex>
-              <Image src={props.src} fallbackSrc='https://via.placeholder.com/150' boxSize='30em'/>
-              <Text ml={3}>
-                {props.description}
-              </Text>
-            </Flex>
-            <Flex mt={2} justify='center'>
-              {Object.keys(aux).map(function(key){
-                return <Image pl={1} src={aux[key]}/>
-              })}
+            <Flex placeItems='center'>
+              <Image src={props.src} fallbackSrc='https://via.placeholder.com/150' boxSize='22em' borderRadius='lg' />
+              <Box display='flex' flexDirection='column'>
+                <Text ml={3} p={30}>
+                  {props.description}
+                </Text>
+                <Flex mt={4} justify='center' >
+                  {Object.keys(aux).map(function (key) {
+                    return <Image pl={1} src={aux[key]} />
+                  })}
+                </Flex>
+                <Link as={props.as} to={props.to} display='flex' justifyContent='center' href={props.link} isExternal mt={5} textDecoration='underline'>See Project
+                </Link>
+              </Box>
             </Flex>
           </DrawerBody>
         </DrawerContent>
