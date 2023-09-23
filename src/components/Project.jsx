50f088e46a8cca1e485d898fe103b13548457e4c
marcelldac <dacmarcell@proton.me>
2023-09-23 03:30:12 -0300

    feat(Project): modify all structure

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,53 +1,43 @@
 import {
   Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  Flex,
   Image,
   Text,
-  useDisclosure,
-  Box,
-  Link,
+  Card,
+  Stack,
+  CardBody,
+  Heading,
+  CardFooter,
 } from "@chakra-ui/react";
 
 export default function Project(props) {
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const size = 'xl';
-
-  let aux = props.skills;
-
   return (
-    <>
-      <Button onClick={() => onOpen()} m={4}>{props.title}</Button>
-      <Drawer onClose={onClose} isOpen={isOpen} size={size}>
-        <DrawerOverlay />
-        <DrawerContent bg='gray.200'>
-          <DrawerCloseButton />
-          <DrawerHeader textAlign='center' fontSize='26'>{props.title}</DrawerHeader>
-          <DrawerBody>
-            <Flex placeItems='center'>
-              <Image src={props.src} fallbackSrc='https://via.placeholder.com/150' boxSize='22em' borderRadius='lg' />
-              <Box display='flex' flexDirection='column'>
-                <Text ml={3} p={30}>
-                  {props.description}
-                </Text>
-                <Flex mt={4} justify='center' >
-                  {Object.keys(aux).map(function (key) {
-                    return <Image pl={1} src={aux[key]} />
-                  })}
-                </Flex>
-                <Link as={props.as} to={props.to} display='flex' justifyContent='center' href={props.link} isExternal mt={5} textDecoration='underline'>See Project
-                </Link>
-              </Box>
-            </Flex>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </>
+    <Card
+      direction={{ base: 'column', sm: 'row' }}
+      overflow='hidden'
+      variant='outline'
+      mt={20}
+    >
+      <Image
+        objectFit='cover'
+        maxW={{ base: '100%', sm: '200px' }}
+        src={props.src}
+        alt='Caffe Latte'
+      />
+      <Stack>
+        <CardBody>
+          <Heading size='md'>{props.title}</Heading>
+          <Text py='2'>
+            {props.description}
+          </Text>
+        </CardBody>
+        <CardFooter>
+          <a href={props.link} target="_blank">
+            <Button variant='solid' colorScheme='green' >
+              See More
+            </Button>
+          </a>
+        </CardFooter>
+      </Stack>
+    </Card>
   )
 }
