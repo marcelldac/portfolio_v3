c81769f5aaf5b8e53a3f8fc4899649cfd66d62eb
marcelldac <dacmarcell@proton.me>
2023-09-23 03:54:06 -0300

    feat: add p9 informations

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,6 +14,7 @@ export default function Projects() {
     p6: 'https://github-production-user-asset-6210df.s3.amazonaws.com/83461178/264456263-f73130fc-c3a5-41a7-bf4c-3d4c460b8284.png',
     p7: 'https://github-production-user-asset-6210df.s3.amazonaws.com/83461178/268435778-07114689-599b-424d-a9d0-a71a7c97b250.png',
     p8: 'https://github-production-user-asset-6210df.s3.amazonaws.com/83461178/268437038-9fbd5a7a-04b4-4bfb-a7f8-042cac07cf70.png',
+    p9: 'https://github-production-user-asset-6210df.s3.amazonaws.com/83461178/270091743-9978722a-e987-4500-b522-8309f00665bf.png',
   }
 
   const desc = {
@@ -25,6 +26,7 @@ export default function Projects() {
     p6: 'A website that generates a thumbnail from a link and provides it for download, built using Python and the Flask framework.',
     p7: 'A zip code finder. The user puts your postal code info and the system returns information about it.',
     p8: 'My old portfolio. Made with ReactJs, it is my 2.0 version of portfolio. (this is 3.0.).',
+    p9: 'The User Management System is a robust Java Spring Boot application designed to streamline the management of user data within an organization. This project implements a full-fledged CRUD (Create, Read, Update, Delete) functionality for user entities, offering a secure and efficient way to handle user information.',
   }
 
   const links = {
@@ -36,10 +38,12 @@ export default function Projects() {
     p6: 'https://github.com/marcelldac/thumbnail-downloader',
     p7: 'https://github.com/marcelldac/zip-code-finder',
     p8: 'https://github.com/marcelldac/portfolio',
+    p9: 'https://github.com/marcelldac/crud_usuarios',
   }
 
   return (
     <Box m={30}>
+      <Project title="Users CRUD" src={img.p9} description={desc.p9} skills={[skills.java]} link={links.p9} />
       <Project title='My Portfolio v2.0' src={img.p8} description={desc.p8} skills={[skills.javascript, skills.react_js]} link={links.p8} />
       <Project title='Zip Code Finder' src={img.p7} description={desc.p7} skills={[skills.javascript, skills.react_js]} link={links.p7} />
       <Project title='React Native Guide' src={img.p1} description={desc.p1} skills={[skills.react_native, skills.javascript]} link={links.p1} />
