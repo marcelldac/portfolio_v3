eb61b5d688683aacc2a71868ab7bf81c3d799035
marcelldac <dacmarcell@proton.me>
2023-09-23 04:50:01 -0300

    feat: modify home page structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
-import { Box, Text, Divider } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Divider,
+  Image,
+  Flex,
+  Center,
+} from "@chakra-ui/react";
 
 import CustomTabs from '../components/CustomTabs';
-
 
 const Title = ({ children }) => {
   return <Text fontSize='30' color='gray.700' m={10}>{children}</Text>
@@ -14,14 +20,23 @@ const Subtitle = ({ children }) => {
 export default function Home() {
   return (
     <Box>
-      <Title>Who am i?</Title>
-      <Subtitle>Marcell. 19 y/o developer, Linux lover and bitcoiner!</Subtitle>
-      {/* TODO: Colocar link do projeto unicripto (direcionar pro projeto dentro do site ou pro site da Unicripto que será criado) */}
-      <Subtitle>Currently working on UniCripto as CEO.</Subtitle>
-      <Subtitle>Currently delving into Java.</Subtitle>
-      <Subtitle>I worked as a Mobile Developer.</Subtitle>
+      <Flex direction='row' justifyContent='space-around'>
+        <Box>
+          <Subtitle >Marcell. 19 y/o developer, Linux lover and bitcoiner!</Subtitle>
+          {/* TODO: Colocar link do projeto unicripto (direcionar pro projeto dentro do site ou pro site da Unicripto que será criado) */}
+          <Subtitle>Currently working on UniCripto as CEO.</Subtitle>
+          <Subtitle>Currently delving into Java.</Subtitle>
+          <Subtitle>Freelancer Developer since 2022!</Subtitle>
+        </Box>
+        <Box textAlign='center'>
+          <Image src='./me.jpg' borderRadius='full' boxSize='150px' fallbackSrc='https://via.placeholder.com/150' justifySelf='stretch' />
+          <Text as='b'>Marcell Dactes</Text>
+          <Text>Full-Stack Developer</Text>
+        </Box>
+      </Flex>
       <Divider my={10} />
       <CustomTabs />
     </Box>
+
   )
 }
