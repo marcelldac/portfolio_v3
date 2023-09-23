2d31912a4b96f9d02bff56c06ecd33fd6ca30ebd
marcelldac <dacmarcell@proton.me>
2023-09-23 03:26:16 -0300

    feat(Skill): modify all strucutre

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,24 +1,19 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text, Tr, Td, Tbody } from "@chakra-ui/react";
 
 export default function Skill(props) {
   return (
-    <>
-      <Box
-        border='1px'
-        borderColor='gray.200'
-        h='20'
-        minW='77em'
-        alignSelf='center'
-        display='flex'
-        alignItems='center'
-        justifyContent='space-around'
-        color='gray.100'
-      >
-        <Image w='100px' src={props.skill} />
-        <Text mr={10} >{props.title}</Text>
-        {/* todo: ajeitar a quantidade */}
-        <Text mr={10}>{props.quantity}</Text>
-      </Box>
-    </>
+    <Tbody>
+      <Tr>
+        <Td>
+          <img src={props.icon} /> {/* {skill.chakraui} */}
+        </Td>
+        <Td>
+          {props.title}{/* ChakraUI */}
+        </Td>
+        <Td>
+          {props.quantity}{/* {skillQuant.chakraui} */}
+        </Td>
+      </Tr >
+    </Tbody>
   )
 }
