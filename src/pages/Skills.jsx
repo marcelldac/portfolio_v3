5dc6049fd61ca402a65abab1117a385339c158c5
marcelldac <dacmarcell@proton.me>
2023-09-23 03:25:04 -0300

    feat(Skills): modify all structure

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tr,
+  Th,
+  TableCaption,
+  TableContainer,
+  Box,
+} from '@chakra-ui/react'
 import axios from "axios";
 
-import Skill from "../components/Skill";
-
+import Skill from '../components/Skill';
 import skill from '../utils/skills';
 
 export default function Skills() {
@@ -46,23 +53,28 @@ export default function Skills() {
   })
 
   return (
-    <>
-      <Box bgColor='gray.700' w={'1300px'} minH={'1000px'} display='flex' flexDirection='column'>
-        <Box color='white' h='10' w='auto' p={2} mx={10} display='flex' alignItems='center' justifyContent='space-around'>
-          <Text>Technology icon</Text>
-          <Text>Technology name</Text>
-          <Text>How many projects?</Text>
-        </Box>
-        <Skill skill={skill.react_native} title='React Native' quantity={skillQuant.reactnative} />
-        <Skill skill={skill.javascript} title='Javascript' quantity={skillQuant.javascript} />
-        <Skill skill={skill.typescript} title='Typescript' quantity={skillQuant.typescript} />
-        <Skill skill={skill.react_js} title='React.js' quantity={skillQuant.reactjs} />
-        <Skill skill={skill.golang} title='Golang' quantity={skillQuant.golang} />
-        <Skill skill={skill.nextjs} title='Next.js' quantity={skillQuant.nextjs} />
-        <Skill skill={skill.chakraui} title='Chakra-UI' quantity={skillQuant.chakraui} />
-        <Skill skill={skill.python} title='Python' quantity={skillQuant.python} />
-        <Skill skill={skill.flask} title='Flask' quantity={skillQuant.flask} />
-      </Box>
-    </>
+    <Box display='flex' justifyContent='center'>
+      <TableContainer>
+        <Table variant='simple'>
+          <TableCaption>Some of my skills.</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Icon</Th>
+              <Th>Name</Th>
+              <Th>How Many Projects?</Th>
+            </Tr>
+          </Thead>
+          <Skill icon={skill.react_native} title='React Native' quantity={skillQuant.reactnative} />
+          <Skill icon={skill.javascript} title='Javascript' quantity={skillQuant.javascript} />
+          <Skill icon={skill.typescript} title='Typescript' quantity={skillQuant.typescript} />
+          <Skill icon={skill.react_js} title='React.js' quantity={skillQuant.reactjs} />
+          <Skill icon={skill.golang} title='Golang' quantity={skillQuant.golang} />
+          <Skill icon={skill.nextjs} title='Next.js' quantity={skillQuant.nextjs} />
+          <Skill icon={skill.chakraui} title='Chakra-UI' quantity={skillQuant.chakraui} />
+          <Skill icon={skill.python} title='Python' quantity={skillQuant.python} />
+          <Skill icon={skill.flask} title='Flask' quantity={skillQuant.flask} />
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
