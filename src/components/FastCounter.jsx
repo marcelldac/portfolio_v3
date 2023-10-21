fc1619f43a03f0fbd83d2683a228358f376bb7ef
marcelldac <dacmarcell@gmail.com>
2023-10-21 20:38:35 -0300

    feat: modify fastCounter

--- src/components/FastCounter.jsx
+++ src/components/FastCounter.jsx
@@ -1,21 +1,25 @@
 import { animate, useMotionValue, useTransform, motion } from "framer-motion";
 import { useEffect } from "react";
-import { Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function FastCounter(props) {
-    const count = useMotionValue(0);
-    const rounded = useTransform(count, Math.round);
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, Math.round);
 
-    useEffect(() => {
-        const animation = animate(count, props.quantity, { duration: 3 });
-        return animation.stop;
-    }, [])
+  useEffect(() => {
+    const animation = animate(count, props.quantity, { duration: 3 });
+    return animation.stop;
+  }, []);
 
-    return (
-        <Text ml={5} color='#BEE3F8'>
-            <motion.h1>
-                {rounded}
-            </motion.h1>
-        </Text>
-    )
+  return (
+    <Flex direction="row" ml={12}>
+      <motion.h1
+        color={props.color || "black"}
+        style={{ fontWeight: 500, fontSize: 16 }}
+      >
+        {rounded}
+      </motion.h1>
+      <Text ml={2}>{props.text}</Text>
+    </Flex>
+  );
 }
